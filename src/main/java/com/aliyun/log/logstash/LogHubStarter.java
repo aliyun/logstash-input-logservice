bbe012e552110df8a941af329de865f0dfb293ea
chaolee50 <chaolee50@outlook.com>
2022-12-20 15:49:29 +0800

    add proxy

--- src/main/java/com/aliyun/log/logstash/LogHubStarter.java
+++ src/main/java/com/aliyun/log/logstash/LogHubStarter.java
@@ -16,10 +16,11 @@ public class LogHubStarter {
     public static void startWorker(String endpoint, String accessId, String accessKey,
                                    String project, String logstore,
                                    String consumerGroup, String consumer, String position,
-                                   int checkpointSecond, boolean includeMeta, BlockingQueue<Map<String, String>> queueCache) throws LogHubClientWorkerException {
+                                   int checkpointSecond, boolean includeMeta, BlockingQueue<Map<String, String>> queueCache,
+                                   String proxyHost, int proxyPort, String proxyUsername, String proxyPassword, String proxyDomain, String proxyWorkstation
+    ) throws LogHubClientWorkerException {
         // 第二个参数是消费者名称，同一个消费组下面的消费者名称必须不同，可以使用相同的消费组名称，
         // 不同的消费者名称在多台机器上启动多个进程，来均衡消费一个Logstore，这个时候消费者名称可以使用机器ip来区分。
-        // 第9个参数（maxFetchLogGroupSize）是每次从服务端获取的LogGroup数目，使用默认值即可，如有调整请注意取值范围(0,1000]
         LogHubConfig config;
         if ("begin".equals(position)) {
             config = new LogHubConfig(consumerGroup, consumer, endpoint, project, logstore, accessId, accessKey, LogHubConfig.ConsumePosition.BEGIN_CURSOR);
@@ -29,9 +30,26 @@ public class LogHubStarter {
             int time = (int) (Timestamp.valueOf(position).getTime() / ONE_SECOND);
             config = new LogHubConfig(consumerGroup, consumer, endpoint, project, logstore, accessId, accessKey, time);
         }
+        if (proxyHost!=null && !proxyHost.isEmpty()) {
+            config.setProxyHost(proxyHost);
+        }
+        if (proxyPort!=0) {
+            config.setProxyPort(proxyPort);
+        }
+        if (proxyUsername!=null && !proxyUsername.isEmpty()) {
+            config.setProxyUsername(proxyUsername);
+        }
+        if (proxyPassword!=null && !proxyPassword.isEmpty()) {
+            config.setProxyPassword(proxyPassword);
+        }
+        if (proxyDomain!=null && !proxyDomain.isEmpty()) {
+            config.setProxyDomain(proxyDomain);
+        }
+        if (proxyWorkstation!=null && !proxyWorkstation.isEmpty()) {
+            config.setProxyWorkstation(proxyWorkstation);
+        }
         ClientWorker worker = new ClientWorker(new LogstashLogHubProcessorFactory(checkpointSecond, includeMeta, consumerGroup+"/"+consumer, queueCache), config);
         Thread thread = new Thread(worker);
-        //Thread运行之后，Client Worker会自动运行，ClientWorker扩展了Runnable接口。
         thread.start();
     }
 }
